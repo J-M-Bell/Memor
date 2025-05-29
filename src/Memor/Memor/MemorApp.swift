36e34e4e77a9660bd90c124880885af02c6d7366
JM Bell <justinmbell1997@gmail.com>
2025-05-29 07:43:03 -0500

    added xcode project

---
+++ src/Memor/Memor/MemorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorApp.swift
+//  Memor
+//
+//  Created by Justin Bell on 5/29/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
