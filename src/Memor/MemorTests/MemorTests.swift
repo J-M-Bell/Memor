6e4d2f97466cacb081d020271d0dbdc9fcee1f49
JM Bell <justinmbell1997@gmail.com>
2025-07-09 21:06:09 -0500

    added unit and ui test files

---
+++ src/Memor/MemorTests/MemorTests.swift
@@ -0,0 +1,16 @@
+//
+//  MemorTests.swift
+//  MemorTests
+//
+//  Created by Justin Bell on 7/9/25.
+//
+
+import Testing
+
+struct MemorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
